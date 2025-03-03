aac8b634081d7082ae87e751362f6b625e27e93f
Jaymark Erojo <jaymarspogs@gmail.com>
2025-03-03 10:32:17 +0800

    Add files via upload

---
+++ Chapter1/SimpleFormsApp.cs
@@ -0,0 +1,52 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            string fNameValue = firstName.Text;
+            string lNameValue = lastName.Text;
+            MessageBox.Show($"Hello World! {fNameValue} {lNameValue}", "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void lastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
